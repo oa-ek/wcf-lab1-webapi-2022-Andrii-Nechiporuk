fc89208367c1b5d644f78154dbb5d3fa3ac90b5a
andrii.nechyporuk <andrii.nechyporuk@oa.edu.ua>
2022-11-28 14:35:54 +0200

    Initial + creating structure

---
+++ KeysShop/KeysShop.Core/KeysShopContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeysShop.Core
+{
+    public class KeysShopContext
+    {
+        public KeysShopContext()
+        {
+        }
+
+        public DbSet<Key> Keys { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
