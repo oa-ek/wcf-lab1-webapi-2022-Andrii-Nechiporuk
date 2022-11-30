56f70b35e15c359bff5652554b152cb7fd31ce03
andrii.nechyporuk <andrii.nechyporuk@oa.edu.ua>
2022-11-30 07:17:40 +0200

    SearckController added

---
+++ KeysShop/KeysShop/Server/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using KeysShop.Core;
+using KeysShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KeysShop.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly KeysRepository keysRepository;
+
+        public SearchController(KeysRepository keysRepository)
+        {
+            this.keysRepository = keysRepository;
+        }
+
+        [HttpGet("searchkeys/")]
+        public async Task<List<Key>> SearchKey(string keyname)
+        {
+            if (String.IsNullOrEmpty(keyname))
+            {
+                return null;
+            }
+            var list = keysRepository.GetKeys();
+            list = list.Where(s => s.Name!.ToLower().Contains(keyname.ToLower())).ToList();
+            return list;
+                
+        }
+    }
+}
